de44c0d64d8ff32dc27def519342ad8da0cbaaab
Alessandro Sebastiani <sebbalex@gmail.com>
2023-04-08 16:15:48 +0200

    fix: cd

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  site: 'https://bikeblog2.sebastiani.io',
 });
