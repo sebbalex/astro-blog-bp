20174b23572f0bfcbcf391006969c9aafec3fe62
Alessandro Sebastiani <sebbalex@gmail.com>
2023-04-13 19:22:58 +0200

    fix: missing translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,17 +10,24 @@ export const defaultLang: Langs = "it";
 export const ui: UILangs = {
   en: {
     "app.changeLocale": "Change language",
-    "nav.home": "Home",
-    "nav.about": "About",
-    "nav.twitter": "Twitter",
     "blog.recent_text": "Recent articles",
     "blog.recent_subtext": "Most recent posts for bikepacking",
+    "footer.company.name": "ACME Industries Ltd.",
+    "footer.company.desc": "Providing reliable tech since 1992",
+    "footer.nav.pages": "Pages",
+    "footer.nav.legal": "Legal",
+    "footer.newsletter.label": "Enter your email addres",
+    "footer.newsletter.button": "Subscribe"
   },
   it: {
     "app.changeLocale": "Change language",
-    "nav.home": "Acceuil",
-    "nav.about": "À propos",
     "blog.recent_text": "Articoli recenti",
     "blog.recent_subtext": "Articoli più recenti per il bikepacking",
+    "footer.company.name": "ACME Industries Ltd.",
+    "footer.company.desc": "Providing reliable tech since 1992",
+    "footer.nav.pages": "Pagine",
+    "footer.nav.legal": "Privacy",
+    "footer.newsletter.label": "Resta aggiornato, inserisci il tuo indirizzo email",
+    "footer.newsletter.button": "Invia"
   },
 } as const;
